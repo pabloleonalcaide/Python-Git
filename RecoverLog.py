19479f26490026663f83ef67be0d399c717e9b85
P Leon <pabloleonpsico@gmail.com>
2017-06-04 19:13:54 +0200

    Update RecoverLog.py

--- RecoverLog.py
+++ RecoverLog.py
@@ -7,11 +7,13 @@ import subprocess
 import os
 import sys
 
+#Recuperamos el registro de operaciones en la carpeta donde tenemos nuestro repositorio local
+operation = subprocess.Popen( "/usr/bin/git log" , 
+	cwd = os.path.dirname( '/home/pablo/workspace_proyecto/' ), 
+	shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE )
+(out, error) = operation.communicate()
 
-pr = subprocess.Popen( "/usr/bin/git log" , cwd = os.path.dirname( '/home/pablo/workspace_proyecto/' ), shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE )
-(out, error) = pr.communicate()
-
-
-print "Error : " + str(error)
-
-print "out : " + str(out)
+#Imprimimos los posibles errores
+print "Error: " + str(error)
+#Imprimimos los registros de log
+print "Registro: " + str(out)
