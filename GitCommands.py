4bf67c6f6bff7f5a152e20e77c5528638f75eaea
P Leon <pabloleonpsico@gmail.com>
2017-06-05 08:52:50 +0200

    added checking of .git directory and comments

--- GitCommands.py
+++ GitCommands.py
@@ -1,33 +1,36 @@
-#Script desde el cual podemos gestionar nuestros repositorios locales de git
-
 __author__ = "Pablo Leon Alcaide"
-#importamos la libreria de git
+#import git and os repository, we need the os repository to check de path
 import git
 import os
 
 print 'Bienvenido al asistente de git'
 
-#indicamos la ruta
+#insert the path
 ruta = raw_input('indica la ruta absoluta del repositorio: ')
-#comprobamos si se ha introducido un directorio (sería prudente comprobar si el directorio tiene un elemento .git)
-if os.path.isdir(ruta):
-    repositorio = git.Repo(ruta)
-    try:
-        opcion = int(raw_input('\nSelecciona una opcion:\n\t1-Comprobar status\n\t2-Realizar commit\n\t3-Salir'))
-        while True:
-            if opcion == 1:
-                #comprobamos el status de nuestro repositorio local
-                print repositorio.git.status()
-            elif opcion == 2:
-                #realizamos el add y su commit correspondiente
-                print repositorio.git.add('.')
-                comentario = raw_input('comentario para el commit: ')
-                print repositorio.git.commit( m=comentario)
-            elif opcion == 3:
-                break
-            opcion = int(raw_input('\nSelecciona una opcion:\n\t1-Comprobar status\n\t2-Realizar commit\n\t3-Salir'))
 
-    except ValueError:
-        print 'Introduciste una opcion incorrecta'
+if os.path.isdir(ruta):
+    #find the .git
+    if os.path.isdir(ruta+'/.git'):
+        print 'repositorio encontrado'
+        repositorio = git.Repo(ruta)
+        try:
+            opcion = int(raw_input('\nSelecciona una opcion:\n\t1-Comprobar status\n\t2-Realizar commit\n\t3-Salir'))
+            while True:
+                if opcion == 1:
+                    #check the status
+                    print repositorio.git.status()
+                elif opcion == 2:
+                    #execute the 'add .' (all) and 'commit -m' commands
+                    print repositorio.git.add('.')
+                    comentario = raw_input('comentario para el commit: ')
+                    print repositorio.git.commit( m=comentario)
+                elif opcion == 3:
+                    break
+                opcion = int(raw_input('\nSelecciona una opcion:\n\t1-Comprobar status\n\t2-Realizar commit\n\t3-Salir'))
+
+        except ValueError:
+            print 'Introduciste una opcion incorrecta'
+    else:
+        print 'repositorio no encontrado en el directorio'
 else:
     print 'No introduciste ninguna ruta'
